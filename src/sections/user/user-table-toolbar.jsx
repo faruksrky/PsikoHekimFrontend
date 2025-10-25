3e5aee8558a96e0859b89975faf89da0ff70e7be
faruksrky <faruksrky@hotmail.com>
2025-10-25 13:45:08 +0300

    fix: Remove TableRow and TableCell from UserTableToolbar to fix DOM nesting warning

--- src/sections/user/user-table-toolbar.jsx
+++ src/sections/user/user-table-toolbar.jsx
@@ -8,8 +8,6 @@ import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
 
 import { Iconify } from 'src/components/iconify';
 import { usePopover, CustomPopover } from 'src/components/custom-popover';
@@ -36,79 +34,60 @@ export function UserTableToolbar({ filters, options, onResetPage }) {
   );
 
   return (
-    <TableRow>
-      {/* colSpan değerini tablondaki sütun sayısına göre ayarlayın */}
-      <TableCell colSpan={6} sx={{ borderBottom: 'none', p: 0 }}>
-        <Stack
-          spacing={2}
-          alignItems={{ xs: 'flex-end', md: 'center' }}
-          direction={{ xs: 'column', md: 'row' }}
-          sx={{ p: 2.5, pr: { xs: 2.5, md: 1 } }}
-        >
-          <FormControl sx={{ flexShrink: 0, width: { xs: 1, md: 200 } }}>
-            <TextField
-              fullWidth
-              label="Adı"
-              value={filters.state.firstName}
-              onChange={handleFilterFirstName}
-            />
-          </FormControl>
+    <Stack
+      spacing={2}
+      alignItems={{ xs: 'flex-end', md: 'center' }}
+      direction={{ xs: 'column', md: 'row' }}
+      sx={{ p: 2.5, pr: { xs: 2.5, md: 1 } }}
+    >
+      <FormControl sx={{ flexShrink: 0, width: { xs: 1, md: 200 } }}>
+        <TextField
+          fullWidth
+          label="Adı"
+          value={filters.state.firstName}
+          onChange={handleFilterFirstName}
+        />
+      </FormControl>
 
-          <FormControl sx={{ flexShrink: 0, width: { xs: 1, md: 200 } }}>
-            <TextField
-              fullWidth
-              label="Soyadı"
-              value={filters.state.lastName}
-              onChange={handleFilterLastName}
-            />
-          </FormControl>
+      <FormControl sx={{ flexShrink: 0, width: { xs: 1, md: 200 } }}>
+        <TextField
+          fullWidth
+          label="Soyadı"
+          value={filters.state.lastName}
+          onChange={handleFilterLastName}
+        />
+      </FormControl>
 
-          <Stack direction="row" alignItems="center" spacing={2} flexGrow={1} sx={{ width: 1 }}>
-            <TextField
-              fullWidth
-              value={filters.state.name}
-              onChange={handleFilterFirstName}
-              placeholder="Ara..."
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
-                  </InputAdornment>
-                ),
-              }}
-            />
+      <Stack direction="row" alignItems="center" spacing={2} flexGrow={1} sx={{ width: 1 }}>
+        <IconButton onClick={popover.onOpen}>
+          <Iconify icon="eva:more-vertical-fill" />
+        </IconButton>
+      </Stack>
 
-            <IconButton onClick={popover.onOpen}>
-              <Iconify icon="eva:more-vertical-fill" />
-            </IconButton>
-          </Stack>
-        </Stack>
+      <CustomPopover
+        open={popover.open}
+        anchorEl={popover.anchorEl}
+        onClose={popover.onClose}
+        slotProps={{ arrow: { placement: 'right-top' } }}
+      >
+        <MenuList>
+          <MenuItem onClick={() => popover.onClose()}>
+            <Iconify icon="solar:printer-minimalistic-bold" />
+            Print
+          </MenuItem>
 
-        <CustomPopover
-          open={popover.open}
-          anchorEl={popover.anchorEl}
-          onClose={popover.onClose}
-          slotProps={{ arrow: { placement: 'right-top' } }}
-        >
-          <MenuList>
-            <MenuItem onClick={() => popover.onClose()}>
-              <Iconify icon="solar:printer-minimalistic-bold" />
-              Print
-            </MenuItem>
+          <MenuItem onClick={() => popover.onClose()}>
+            <Iconify icon="solar:import-bold" />
+            Import
+          </MenuItem>
 
-            <MenuItem onClick={() => popover.onClose()}>
-              <Iconify icon="solar:import-bold" />
-              Import
-            </MenuItem>
-
-            <MenuItem onClick={() => popover.onClose()}>
-              <Iconify icon="solar:export-bold" />
-              Export
-            </MenuItem>
-          </MenuList>
-        </CustomPopover>
-      </TableCell>
-    </TableRow>
+          <MenuItem onClick={() => popover.onClose()}>
+            <Iconify icon="solar:export-bold" />
+            Export
+          </MenuItem>
+        </MenuList>
+      </CustomPopover>
+    </Stack>
   );
 }
 // ...existing code...
