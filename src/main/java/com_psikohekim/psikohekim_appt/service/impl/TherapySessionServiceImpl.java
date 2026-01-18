5934f3e542cc32116e826a5d7e3209ba2640fa31
faruksrky <faruksrky@hotmail.com>
2026-01-18 18:31:48 +0300

    Remove _redirects file completely - use Functions middleware only

--- src/main/java/com_psikohekim/psikohekim_appt/service/impl/TherapySessionServiceImpl.java
+++
@@ -1,4 +0,0 @@
-import com_psikohekim.psikohekim_appt.mapper.TherapySessionMapper;
-import com_psikohekim.psikohekim_appt.model.TherapySession;
-import com_psikohekim.psikohekim_appt.model.Therapist;
-import com_psikohekim.psikohekim_appt.model.TherapistPatient;
